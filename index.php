27443af26aeef444d457e46b5ecc17880335bcf3
DevOps.Paul <tapadippaul@gmail.com>
2023-08-27 20:47:27 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <title>News Daily</title>
+  <meta name="description" content="" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" />
+  <link rel="stylesheet" href="css/components/sidebar.css" />
+  <link rel="stylesheet" href="css/components/home.css" />
+  <link rel="stylesheet" href="css/components/about.css" />
+  <link rel="stylesheet" href="css/components/news.css" />
+  <link rel="stylesheet" href="css/components/contact.css" />
+  <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+  <header class="header container">
+    <nav>
+      <ul class="header__sidebar">
+        <h2 class="sidebar__title">News Daily</h2>
+        <li>
+          <a class="header__sidebar-links" href="#home"><i class="bi bi-house-door-fill"></i>Home</a>
+        </li>
+        <li>
+          <a class="header__sidebar-links" href="#about"><i class="bi bi-question-circle-fill"></i>About</a>
+        </li>
+        <li>
+          <a class="header__sidebar-links" href="#news"><i class="bi bi-newspaper"></i>News</a>
+        </li>
+        <li>
+          <a class="header__sidebar-links" href="#contact"><i class="bi bi-person-lines-fill"></i>Contact</a>
+        </li>
+        <li>
+          <a class="header__sidebar-links" href="admin-form/admin.php" target="_blank"><i class="bi bi-person-circle"></i>Register</a>
+        </li>
+        <li>
+          <a class="header__sidebar-links" href="login-form/login.php" target="_blank"><i class="bi bi-person-fill-check"></i>Login</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <section class="home container" id="home">
+      <div class="home-container">
+        <h1 class="home__heading">Home</h1>
+        <img class="home__img" src="images/home.png" alt="" />
+        <h1 class="home__title">
+          WELCOME <br />
+          TO <br />
+          NEWS DAILY
+        </h1>
+        <h4 class="home__subtitle">Welcome to Our News Portal.</h4>
+      </div>
+    </section>
+    <section class="about container" id="about">
+      <div class="about-container">
+        <h1 class="about__heading">About Us</h1>
+        <p class="about__description">
+          Welcome to <span>News Daily</span>, your trusted source for
+          up-to-date and reliable news from around the world. Our dedicated
+          team of journalists and contributors strives to deliver accurate and
+          unbiased information to our readers, ensuring they stay
+          well-informed on the latest happenings across various topics. At
+          <span>News Daily</span>, we believe in the power of knowledge and
+          the importance of a well-informed society. Our mission is to provide
+          comprehensive coverage of local, national, and international news,
+          while maintaining the highest standards of journalism ethics. We aim
+          to foster critical thinking, encourage meaningful discussions, and
+          promote a better understanding of the world we live in. We strive to
+          cover a wide range of topics, including politics, business,
+          technology, science, entertainment, and more. Our goal is to provide
+          balanced and comprehensive coverage that goes beyond headlines,
+          delving into the underlying context and implications of each story.
+          We aim to present facts objectively, avoiding sensationalism or
+          bias, and allowing our readers to form their own opinions based on
+          accurate information. We value the trust of our readers and
+          understand the importance of maintaining transparency. We have a
+          clear editorial policy and strive to correct any inaccuracies
+          promptly. We also welcome feedback and encourage our readers to
+          engage with us, fostering a sense of community and dialogue.
+          <br />
+          Thank you for choosing <span>News Daily</span> as your news source.
+          We are dedicated to serving you with the highest quality journalism
+          and look forward to being your go-to platform for news and analysis.
+        </p>
+      </div>
+    </section>
+    <section class="news container" id="news">
+      <div class="news-container">
+        <h1 class="news__heading">News</h1>
+        <div class="news_s-fb">
+          <img class="football-img" src="images/football.jpg" alt="" />
+          <div class="football-description">
+            <a class="football-news" href="news-pages/news_s/sports-content/news-content.html" target="_blank">Why the
+              English Championship is the best sporting
+              competition?</a>
+            <p class="fb-news_description">
+              Coventry City haven't been in the Premier League since the turn
+              of the century, but they're 90 minutes from <br />
+              returning to England's top flight.
+            </p>
+          </div>
+        </div>
+        <div class="news_s-ck">
+          <img class="cricket-img" src="images/cricket.jpg" alt="" />
+          <div class="cricket-description">
+            <a class="cricket-news" href="news-pages/news_s/sports-content/news-content.html" target="_blank">IPL 2023
+              comes full circle as wily CSK face defending champs
+              Titans</a>
+            <p class="ck-news_description">
+              We are back to where we began, with one team looking to
+              embellish a fab record and the other eyeing a <br />
+              new era of dominance.
+            </p>
+          </div>
+        </div>
+        <div class="news_s-f2">
+          <img class="f2-img" src="images/f2.jpg" alt="" />
+          <div class="f2-description">
+            <a class="f2-news" href="news-pages/news_s/sports-content/news-content.html" target="_blank">F2: Iwasa takes
+              Drivers’ Championship lead with Monaco Sprint
+              win</a>
+            <p class="f2-news_description">
+              Ayumu Iwasa claimed his third win of the 2023 Formula 2 season,
+              winning the Sprint Race from P2 on <br />
+              the grid in Monaco.
+            </p>
+          </div>
+        </div>
+        <div class="news_s-mma">
+          <img class="mma-img" src="images/mma.jpg" alt="" />
+          <div class="mma-description">
+            <a class="mma-news" href="news-pages/news_s/sports-content/news-content.html" target="_blank">MMA News
+              Today: Tyson Fury Dismisses Dana White's Proposal To
+              <br />
+              Fight Jon Jones, Mike Perry Inserts Himself Into Jake Paul vs
+              Nate Diaz</a>
+            <p class="mma-news_description">
+              Tyson Fury makes stance on a fight with Jon Jones clear, Mike
+              Perry angles for involvement in <br />
+              Jake Paul vs. Nate Diaz, and more.
+            </p>
+          </div>
+        </div>
+        <div class="news_s-moto-gp">
+          <img class="moto-gp-img" src="images/motogp.jpg" alt="" />
+          <div class="moto-gp-description">
+            <a class="moto-gp-news" href="news-pages/news_s/sports-content/news-content.html" target="_blank">Valentino
+              Rossi lists his fiercest MotoGP rivals - but ignores
+              one name</a>
+            <p class="moto-gp-news_description">
+              Valentino Rossi opted to leave Marc Marquez out when asked to
+              name his nastiest MotoGP rivalries.
+            </p>
+          </div>
+        </div>
+        <div class="news_s-debt">
+          <img class="debt-img" src="images/debt.jpg" alt="" />
+          <div class="debt-description">
+            <a class="debt-news" href="news-pages/news_s/political-content/news-content.html" target="_blank">Here’s
+              what’s in the debt ceiling deal</a>
+            <p class="debt-news_description">
+              After several weeks of tense negotiations, President Joe Biden and House Republicans have reached an
+              agreement in <br /> principle to raise the debt ceiling and cap spending.
+            </p>
+          </div>
+        </div>
+        <div class="news_s-presidential">
+          <img class="presidential-img" src="images/presidential.jpg" alt="" />
+          <div class="presidential-description">
+            <a class="presidential-news" href="news-pages/news_s/political-content/news-content.html" target="_blank">What to expect from a Ron DeSantis presidential campaign</a>
+            <p class="presidential-news_description">
+              Florida Gov. Ron DeSantis is expected to make his presidential campaign official next week.
+            </p>
+          </div>
+        </div>
+        <div class="news_s-voters">
+          <img class="voters-img" src="images/voters.jpg" alt="" />
+          <div class="voters-description">
+            <a class="voters-news" href="news-pages/news_s/political-content/news-content.html" target="_blank">The
+              demographic makeup of the country’s voters continues to shift. <br /> That creates headwinds for
+              Republicans</a>
+            <p class="voters-news_description">
+              Demographic change continued to chip away at the cornerstone of the Republican electoral <br /> coalition
+              in 2022, a new analysis of Census data has found.
+            </p>
+          </div>
+        </div>
+        <div class="news_s-ind">
+          <img class="ind-img" src="images/ind.jpg" alt="" />
+          <div class="ind-description">
+            <a class="ind-news" href="news-pages/news_s/political-content/news-content.html" target="_blank">PM Modi
+              inaugurates new Parliament building, installs sengol in <br /> Lok Sabha chamber</a>
+            <p class="ind-news_description">
+              Twenty opposition parties boycott the ceremony as the PM installs the sengol next to the chair of <br />
+              Speaker amid vedic chants
+            </p>
+          </div>
+        </div>
+        <div class="news_s-parliament">
+          <img class="parliament-img" src="images/parliament.jpg" alt="" />
+          <div class="parliament-description">
+            <a class="parliament-news" href="news-pages/news_s/political-content/news-content.html" target="_blank">New
+              Parliament Building: Opposition boycott signals collective <br /> departure from democratic values</a>
+            <p class="parliament-news_description">
+              The Opposition’s churlish boycott of the inauguration of the new Parliament <br /> building is on very
+              weak ground.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="contact container" id="contact">
+      <div class="contact-container">
+        <h1 class="contact__heading">Contact Us</h1>
+        <div class="contact-sec">
+          <div class="contact-loc">
+            <li class="contact-icon"><i class="bi bi-geo-alt-fill"></i></li>
+            <h4 class="contact-text">Location</h4>
+            <p class="contact-info">Raniganj</p>
+          </div>
+          <div class="contact-no">
+            <li class="contact-icon"><i class="bi bi-telephone-fill"></i></li>
+            <h4 class="contact-text">Contact</h4>
+            <p class="contact-info">+91 9064484071</p>
+          </div>
+          <div class="contact-email">
+            <li class="contact-icon"><i class="bi bi-envelope-at-fill"></i></li>
+            <h4 class="contact-text">Email</h4>
+            <p class="contact-info">tapadippaul@gmail.com</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</body>
+
+</html>
